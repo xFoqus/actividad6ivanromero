358152b98876a6efffb609c267e542ee1efd69f1
Ivan <ivanrhaut@gmail.com>
2024-09-08 17:04:35 +0200

    Creamos lo necesario para empezar a maquetar

---
+++ src/app/pages/newuser/newuser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newuser',
+  standalone: true,
+  imports: [],
+  templateUrl: './newuser.component.html',
+  styleUrl: './newuser.component.css'
+})
+export class NewuserComponent {
+
+}
---
+++ src/app/pages/updateuser/updateuser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateuser',
+  standalone: true,
+  imports: [],
+  templateUrl: './updateuser.component.html',
+  styleUrl: './updateuser.component.css'
+})
+export class UpdateuserComponent {
+
+}
